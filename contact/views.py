e5cba912300a2ceddb0009a62a91a30d6b34bc4f
Alice Chan <alicebay10@yahoo.co.uk>
2024-10-03 16:03:31 +0000

    Added functionality to contact for, so the messages can send to the sit admin and site admin can view the message.

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.contrib import messages
 from .forms import ContactForm
+
+# Create your views here.
 
 def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
+            form.save()
+            contact_form = ContactForm()
+            messages.add_message(request, messages.SUCCESS, "Message received! I endeavour to respond within 2 working days.")
             # Process the form data
             # For example, send an email or save to the database
-            return redirect('success')
+            
     else:
         form = ContactForm()
     return render(request, 'contact/contact.html', {'form': form})
