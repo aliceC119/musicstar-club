a06e1751898ebf57b46a29a6ef8e0c777cc1a813
Alice Chan <alicebay10@yahoo.co.uk>
2024-10-20 09:50:56 +0000

    updated Site ID setting.

--- musicstar_club/settings.py
+++ musicstar_club/settings.py
@@ -59,10 +59,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
-)
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
