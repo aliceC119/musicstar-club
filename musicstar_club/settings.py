afa837cd3128afc7953f4a3ce0918ffc312a4c16
Alice Chan <alicebay10@yahoo.co.uk>
2024-10-20 11:01:56 +0000

    updated Site ID setting & updated styles to background color and social share button colour.

--- musicstar_club/settings.py
+++ musicstar_club/settings.py
@@ -59,6 +59,10 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
