92bbb74c212759bd07f9609040dc930003d8d0b1
Alice Chan <alicebay10@yahoo.co.uk>
2024-10-30 15:20:24 +0000

    deleted spaces in the setting.py file after a pep8 check up.

--- musicstar_club/urls.py
+++ musicstar_club/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from django.conf import settings
-#from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,8 +27,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
     path("contact/", include("contact.urls"), name="contact-urls"),
-    
-    
-    
-] 
-#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+]
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
