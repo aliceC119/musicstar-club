ca51dfaa2c39f6998809ec2a31b5b103739c43ed
Alice Chan <alicebay10@yahoo.co.uk>
2024-10-30 10:56:53 +0000

    added a test for view.py in Blog app.

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+# Register your models here.
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
@@ -11,5 +12,5 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
-# Register your models here.
+
 admin.site.register(Comment)
